243f12178efbc2a8e9feb11a5ab4667e2e25717f
Dana <lucasdana71@gmail.com>
2021-11-10 09:38:07 -0500

    nn

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Dana Lucas on 11/10/21.
+//
+
+import Foundation
+
+struct Game {
+var word: String
+var incorrectMovesRemaining: Int
+}
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var listOfWords = ["script", "java", "python","javascript","mali","ruby"]
+    
+    let incorrectMovesAllowed = 7
+    
+    var totalWins = 0
+    var totalLosses = 0
+    
+    @IBOutlet var LetterButton: [UIButton]!
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var correctWordLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        newRound()
         // Do any additional setup after loading the view.
     }
+   
+    var currentGame: Game!
 
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining:incorrectMovesAllowed)
+        updateUI()
+    }
 
+    func updateUI() {
+    scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+    treeImageView.image = UIImage(named: "Tree  \(currentGame.incorrectMovesRemaining)")
+
+    }
+
+    @IBAction func ButtonTapped(_ sender: UIButton) {
+        
+        sender.isEnabled = false
+    }
+    
 }
 
