040ad8f21b11bdbcc0196f606b1070002422c0da
Dana <lucasdana71@gmail.com>
2021-11-11 09:35:37 -0500

    apple pie finished

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -12,8 +12,16 @@ var listOfWords = ["script", "java", "python","javascript","mali","ruby"]
     
     let incorrectMovesAllowed = 7
     
-    var totalWins = 0
-    var totalLosses = 0
+    var totalWins = 0{
+        didSet{
+            newRound()
+        }
+    }
+    var totalLosses = 0{
+        didSet{
+            newRound()
+        }
+}
     
     @IBOutlet var LetterButton: [UIButton]!
     @IBOutlet weak var treeImageView: UIImageView!
@@ -27,22 +35,68 @@ var listOfWords = ["script", "java", "python","javascript","mali","ruby"]
    
     var currentGame: Game!
 
-    func newRound() {
-        let newWord = listOfWords.removeFirst()
-        currentGame = Game(word: newWord, incorrectMovesRemaining:incorrectMovesAllowed)
-        updateUI()
-    }
 
+    
     func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+        letters.append(String(letter))
+        }
+    
+    let wordWithSpacing = letters.joined(separator: " ")
+     correctWordLabel.text = wordWithSpacing
+
     scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
-    treeImageView.image = UIImage(named: "Tree  \(currentGame.incorrectMovesRemaining)")
+    treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
 
     }
+
+    
+
+func updateGameState() {
+if currentGame.incorrectMovesRemaining == 0 {
+totalLosses += 1
+    }
+else if currentGame.word == currentGame.formattedWord {
+totalWins += 1
+    }
+else {
+updateUI()
+    }
+     }
 
     @IBAction func ButtonTapped(_ sender: UIButton) {
-        
         sender.isEnabled = false
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateUI()
+        updateGameState()
+
+
     }
+    
+    func enableLetterButton(_ enable: Bool) {
+    for button in LetterButton {
+    button.isEnabled = enable
+    }
+    }
+    
+    func newRound() {
+    if !listOfWords.isEmpty {
+    let newWord = listOfWords.removeFirst()
+    currentGame = Game(word: newWord, incorrectMovesRemaining:
+    incorrectMovesAllowed, guessedLetters: [])
+    enableLetterButton(true)
+    updateUI()
+    }
+    else {
+    enableLetterButton(false)
+    }
+    }
+
+   
+
     
 }
 
